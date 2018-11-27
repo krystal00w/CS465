732124140bd26b8f970c4decdd88f7817661edc4
seathice <jzhao67@illinois.edu>
2018-11-26 22:26:27 -0600

    Initial commit of base code

---
+++ TBBT/app/src/main/java/edu/illinois/cs465/tbbt/MainActivity.java
@@ -0,0 +1,86 @@
+package edu.illinois.cs465.tbbt;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.support.v7.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+    final Fragment fragment_order = new OrderFragment();
+    final Fragment fragment_tab = new TabFragment();
+    final Fragment fragment_discover = new DiscoverFragment();
+    final FragmentManager fm = getSupportFragmentManager();
+    Fragment active = fragment_order;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Sets up the action bar
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // Sets up the bottom navigation bar
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        fm.beginTransaction().add(R.id.main_container, fragment_discover).hide(fragment_discover).commit();
+        fm.beginTransaction().add(R.id.main_container, fragment_tab).hide(fragment_tab).commit();
+        fm.beginTransaction().add(R.id.main_container,fragment_order).commit();
+    }
+
+    // Bottom navigation bar fragment selection
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_order:
+                    fm.beginTransaction().hide(active).show(fragment_order).commit();
+                    active = fragment_order;
+                    return true;
+
+                case R.id.navigation_tab:
+                    fm.beginTransaction().hide(active).show(fragment_tab).commit();
+                    active = fragment_tab;
+                    return true;
+
+                case R.id.navigation_discover:
+                    fm.beginTransaction().hide(active).show(fragment_discover).commit();
+                    active = fragment_discover;
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.action_bar, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_settings) {
+            Fragment fragment_settings = new SettingsFragment();
+            fm.beginTransaction().replace(R.id.main_container, fragment_settings).addToBackStack(null).commit();
+            return true;
+        }
+
+        // If we got here, the user's action was not recognized, invoke the superclass to handle it.
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ TBBT/app/src/main/java/edu/illinois/cs465/tbbt/TabFragment.java
@@ -0,0 +1,20 @@
+package edu.illinois.cs465.tbbt;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class TabFragment extends Fragment {
+    public TabFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_tab, container, false);
+    }
+}
