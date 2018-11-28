8b7d7d419b675b8bf666c85edd2f9f158bf5fb4b
tbarkaev <tbarka2@illinois.edu>
2018-11-27 19:56:03 -0600

    just testing git config out, nothing substantial

--- TBBT/app/src/main/java/edu/illinois/cs465/tbbt/Discover/BarPreviewFragment.java
+++ TBBT/app/src/main/java/edu/illinois/cs465/tbbt/Discover/BarPreviewFragment.java
@@ -23,7 +23,6 @@ public class BarPreviewFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         View v = null;
         //((MainActivity)getActivity()).setActive(this);
         bar = getArguments().getString("bar");
