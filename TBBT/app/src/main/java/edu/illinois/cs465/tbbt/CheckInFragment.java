047bb2477b3093efdad5bc921e1013967f080ec6
krystalzx <krystal_w@wirelessprv-10-195-72-232.near.illinois.edu>
2018-11-28 17:57:15 -0600

    updates for pickup

---
+++ TBBT/app/src/main/java/edu/illinois/cs465/tbbt/CheckInFragment.java
@@ -0,0 +1,47 @@
+package edu.illinois.cs465.tbbt;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.view.View.OnClickListener;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+
+public class CheckInFragment extends Fragment {
+    public CheckInFragment() {
+        // Required empty public constructor
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_check_in, container, false);
+    }
+
+    // On click of the checked button in the Check-In screen, go to OrderFragment
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        Button showOrderFragment=(Button)getView().findViewById(R.id.checked);
+        showOrderFragment.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                FragmentManager fragmentManager = getFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+                OrderFragment orderFrag = new OrderFragment();
+                fragmentTransaction.replace(R.id.checkin, orderFrag);
+
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+
+            }
+        });
+
+    }
+}
