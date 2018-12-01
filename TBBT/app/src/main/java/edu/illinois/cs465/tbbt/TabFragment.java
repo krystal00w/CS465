7f8c3c38ad38c834c3345be7568efc3dbc73179c
rossch2 <rosssschneider2@gmail.com>
2018-12-01 17:19:27 -0600

    Added list views

--- TBBT/app/src/main/java/edu/illinois/cs465/tbbt/TabFragment.java
+++ TBBT/app/src/main/java/edu/illinois/cs465/tbbt/TabFragment.java
@@ -9,6 +9,9 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
 
 public class TabFragment extends Fragment {
     public TabFragment() {
@@ -19,7 +22,58 @@ public class TabFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = null;
-        if (((MainActivity)getActivity()).getStage() == -1){
+        view = inflater.inflate(R.layout.fragment_tab, container, false);
+
+        ((TextView)view.findViewById(R.id.drink_ready_name)).setText(((MainActivity)getActivity()).getDrinkOneName());
+        ((TextView)view.findViewById(R.id.in_progress_name)).setText(((MainActivity)getActivity()).getDrinkTwoName());
+        ((TextView)view.findViewById(R.id.completed_name)).setText(((MainActivity)getActivity()).getDrinkTwoName());
+
+        ListView listView1 = (ListView)view.findViewById(R.id.completed_list);
+        String[] completed_drinks = {"Drink 2  x1  $2.99"};
+
+        ArrayAdapter<String> adapter1 = new ArrayAdapter<String>(
+            getActivity(),
+            android.R.layout.simple_list_item_1,
+            completed_drinks
+        );
+
+        ListView listView2 = (ListView)view.findViewById(R.id.ready_list);
+        String[] ready_drinks = {"Drink 1  x1  $2.99", "Drink 4 x1 $2.99"};
+
+        ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(
+                getActivity(),
+                android.R.layout.simple_list_item_1,
+                ready_drinks
+        );
+
+        ListView listView3 = (ListView)view.findViewById(R.id.in_progress_list);
+        String[] in_progress_drinks = {"2m Drink 3  x1  $2.99"};
+
+        ArrayAdapter<String> adapter3 = new ArrayAdapter<String>(
+                getActivity(),
+                android.R.layout.simple_list_item_1,
+                in_progress_drinks
+        );
+
+        listView1.setAdapter(adapter1);
+        listView2.setAdapter(adapter2);
+        listView3.setAdapter(adapter3);
+
+        final Button pick_up_button = view.findViewById(R.id.pick_up_button);
+        pick_up_button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+                PickupFragment new_frag = new PickupFragment();
+                FragmentTransaction fragmentTransaction = getActivity().getSupportFragmentManager().beginTransaction();
+                fragmentTransaction.replace(R.id.main_container, new_frag);
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+            }
+        });
+
+        return view;
+
+        /*if (((MainActivity)getActivity()).getStage() == -1){
             view = inflater.inflate(R.layout.fragment_not_checked_in, container, false);
         }
 
@@ -77,7 +131,7 @@ public class TabFragment extends Fragment {
         }
 
         // Inflate the layout for this fragment
-        return view;
+        return view;*/
     }
 
     @Override
