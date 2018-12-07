cccb0caa465c69460c17a615349b9364fc72be1b
krystalzx <krystal_w@wirelessprv-10-195-72-232.near.illinois.edu>
2018-12-07 15:18:55 -0600

    update check-in screen and styles

--- TBBT/app/src/main/java/edu/illinois/cs465/tbbt/PickupFragment.java
+++ TBBT/app/src/main/java/edu/illinois/cs465/tbbt/PickupFragment.java
@@ -6,25 +6,70 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.view.View.OnClickListener;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 
+import java.util.ArrayList;
+import android.widget.ListView;
+
+import edu.illinois.cs465.tbbt.OrderMemory.Drink;
+
 public class PickupFragment extends Fragment {
+
+    public final static String DATA_TYPE = "DataType";
+//    private ArrayList<String> drinks = new ArrayList<>();
+
+
     public PickupFragment() {
         // Required empty public constructor
     }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+//        // pass arguments to a string array [drinks]
+//        if (getArguments() != null){
+//            drinks = getArguments().getStringArrayList(MainActivity.LISTVIEW_DATA);
+//        }
+
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_pickup, container, false);
 
+        ListView listView = (ListView) view.findViewById(R.id.ready_list);
+        ArrayList<Drink> ready = ((MainActivity)getActivity()).getReady();
+
+        String[] ready_drinks = new String[ready.size()];
+        for(int i = 0; i < ready.size(); i++){
+            Drink drink = ready.get(i);
+            String name = drink.drinkName;
+            int quantity = drink.quantity;
+            ready_drinks[i] = name + "\t\t\tx" + quantity;
+        }
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                getActivity(),
+                android.R.layout.simple_list_item_1,
+                ready_drinks
+        );
+        listView.setAdapter(adapter);
+
+
         final Button pickup = view.findViewById(R.id.pickup);
         pickup.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                ((MainActivity)getActivity()).pickUpDrinks();
+//                ((MainActivity)getActivity()).incStage();
+                ((MainActivity)getActivity()).setCheckedIn(false);
+
                 // Code here executes on main thread after user presses button
                 TabFragment new_frag = new TabFragment();
                 FragmentTransaction fragmentTransaction = getActivity().getSupportFragmentManager().beginTransaction();
